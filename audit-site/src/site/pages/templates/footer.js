38bb45838aa9e157f76243fe443acd1cc9efb3b2
Ceki Gulcu <ceki@qos.ch>
2011-10-24 23:08:16 +0200

    preparing release 0.5

--- audit-site/src/site/pages/templates/footer.js
+++ audit-site/src/site/pages/templates/footer.js
@@ -1,4 +1,26 @@
 
-document.write('<p class="footer">')
-document.write('Copyright &copy; 2007-2008  <a href="http://www.qos.ch/">QOS.ch</a>')
-document.write('</p>')
+
+document.write('<table class="footer" border="0">')
+
+document.write('<tr>')
+
+document.write('<td valign="top">Copyright &copy; 2011  <a href="http://www.qos.ch/">QOS.ch</a></td>')
+
+document.write('  <td rowspan="2">')
+document.write('    <script type="text/javascript" src="http://www.ohloh.net/p/586885/widgets/project_users.js"></script>')
+document.write('  </td>')
+
+
+document.write('</tr>')
+
+AAT = '@'
+DOOTT = '.'
+document.write('<tr>') 
+
+document.write('<td align="left" colspan="1">') 
+document.write('We are actively looking for volunteers to proofread the documentation. Please send your corrections or suggestions for improvement to "corrections' + AAT +'qos'+DOOTT+'ch". See also the <a href="http://articles.qos.ch/contributing.html">instructions for contributors</a>.');
+document.write('</td>') 
+document.write('  <td>&nbsp;</td>')
+document.write('</tr>') 
+
+document.write('</table>')
