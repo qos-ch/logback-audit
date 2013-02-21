7d5d206a106c51cbc1c04e311cf719072e616ae6
Ceki Gulcu <ceki@qos.ch>
2013-02-21 15:49:10 +0100

    minor edits

--- audit-site/src/site/pages/templates/left.js
+++ audit-site/src/site/pages/templates/left.js
@@ -8,6 +8,8 @@ document.write('<p class="menu"><a href="' + prefix + 'news.html">News</a></p>')
 document.write('<p class="menu_header">Support</p>');
 document.write('<p class="menu"><a href="' + prefix + 'mailinglist.html">Mailing Lists</a></p>');
 document.write('<p class="menu"><a href="' + prefix + 'bugreport.html">Bug Report</a></p>');
+document.write('<p class="menu"><a href="http://github.com/qos-ch/logback-audit">Source Repository</a></p>');
+
 document.write('</p>');
 
 
