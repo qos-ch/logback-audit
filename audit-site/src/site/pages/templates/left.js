49292b9b5729a9b7fa8a2493517bbfcdea6dd552
Ceki Gulcu <ceki@qos.ch>
2013-02-21 15:57:54 +0100

    align css files with main logback site

--- audit-site/src/site/pages/templates/left.js
+++ audit-site/src/site/pages/templates/left.js
@@ -1,4 +1,5 @@
 
+document.write('<div class="menuGroup">');
 document.write('<p class="menu_header">Logback-Audit project</p>');
 document.write('<p class="menu"><a href="' + prefix + 'index.html">Introduction</a></p>');
 document.write('<p class="menu"><a href="' + prefix + 'download.html">Download</a></p>');
@@ -9,6 +10,7 @@ document.write('<p class="menu_header">Support</p>');
 document.write('<p class="menu"><a href="' + prefix + 'mailinglist.html">Mailing Lists</a></p>');
 document.write('<p class="menu"><a href="' + prefix + 'bugreport.html">Bug Report</a></p>');
 document.write('<p class="menu"><a href="http://github.com/qos-ch/logback-audit">Source Repository</a></p>');
+document.write('</div>');
 
 document.write('</p>');
 
